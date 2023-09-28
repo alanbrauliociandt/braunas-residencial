b840e5b6d6a27dbe0b98484954fa7a80f6c7ce68
Alan Braulio <alan-braulio@hotmail.com>
2023-09-28 17:19:26 -0300

    text adjustments

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -32,31 +32,31 @@ export function About() {
             <p className="text-about">
               {" "}
               Totalmente pensado em tudo que você e sua família merecem; onde o
-              conforto<br/> encontra a conveniência; a tranquilidade se une à
-              modernidade; em um ambiente<br/> que combina a serenidade da Natureza
+              conforto encontra a conveniência; a tranquilidade se une à
+              modernidade; em um ambiente que combina a serenidade da Natureza
               com a comodidade urbana.{" "}
             </p>
             <br />
 
             <p className="text-about">
               Torre única, com apês de 02 e 03 quartos (opções com suíte), em um
-              Condomínio<br/>com atmosfera exclusiva e intimista e lazer que
-              contempla uma piscina convidativa,<br/>academia, espaço
-              gourmet, quadra society, salão de festas, playground, pet place,<br/>
+              Condomínio com atmosfera exclusiva e intimista e lazer que
+              contempla uma piscina convidativa, academia, espaço
+              gourmet, quadra society, salão de festas, playground, pet place,
               tudo isso em um espaço repleto de Natureza!!!{" "}
             </p>
             <br />
 
             <p className="text-about">
-              Mais do que um lugar para morar: o Residencial Braunas Park lhe
-              proporciona<br/> um estilo de vida distinto, onde poderá compartilhar
-              momentos especiais com<br/> quem você ama.{" "}
+              Mais do que um lugar para morar, o Residencial Braúnas Park lhe
+              proporciona um estilo de vida distinto, onde poderá compartilhar
+              momentos especiais com quem você ama.{" "}
             </p>
             <br />
 
             <p className="text-about">
-              Tudo isso com a garantia <strong>CAIXA ECONOMICA FEDERAL</strong>
-              e do Programa Minha Casa<br/> Minha Vida com as melhores condições de
+              Tudo isso com a garantia <strong>CAIXA ECONOMICA FEDERAL </strong>
+              e do Programa Minha Casa Minha Vida com as melhores condições de
               financiamento!
             </p>
           </div>
